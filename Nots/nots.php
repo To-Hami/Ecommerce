1d7f1bae771c746218dd3fc039287a5343f2ff2e
Tohami <72935125+To-Hami@users.noreply.github.com>
2022-08-01 11:42:40 -0700

    dd

--- Nots/nots.php
+++ Nots/nots.php
@@ -6,6 +6,18 @@
 /****************  composer require laravel/ui
 /****************  php artisan ui bootstrap --auth  ****/
 
+
+
+
+/******************** fix error npm  ******************************/
+
+
+/***************** rm node_modules
+/****************  rm package-lock.json  
+/*****************  npm clear cache --force
+/*****************  npm install
+/******************  npm run dev
+
 /**************************************  request link **********************
  *
  *  {{request()->routeIs('detail')?' bg-light':null}}****/
